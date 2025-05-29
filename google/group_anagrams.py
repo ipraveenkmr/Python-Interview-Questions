278d7ebc1504175f269986fac50b8951eed3b051
Praveen <devpraveenkr@gmail.com>
2025-05-30 03:49:12 +0530

    updated

--- google/group_anagrams.py
+++ google/group_anagrams.py
@@ -2,8 +2,10 @@ from collections import defaultdict
 
 def group_anagrams(strs):
     anagrams = defaultdict(list)
+    
     for word in strs:
         key = tuple(sorted(word))
+        print('key ', key)
         anagrams[key].append(word)
     return list(anagrams.values())
 
